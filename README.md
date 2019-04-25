81a6c29cce523a26378fee12bf2675aa4b296e4b
santipm29 <santipmartinez@outlook.com>
2019-04-25 12:06:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# QUEUE SQS FIFO - NODEJS
+
+## installation
+
+```bash
+npm install 
+```
+## Configuration
+
+edit the config.json file
+
+## Test
+
+```bash
+node index.js 
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+const aws = require('aws-sdk');
+const fs = require('fs');
+const uuid = require('uuid/v1');
+const config = JSON.parse(fs.readFileSync('config.json'));
+
+aws.config.update(config.awsUpdate);
+let sqs = new aws.SQS(config.apiVersion);
+
+const sendMessage = (message) => {
+  
+  let params = {
+    DelaySeconds: config.delaySeconds,
+    MessageGroupId: config.messageGroupId,
+    MessageDeduplicationId: uuid(),
+    MessageBody: message,
+    QueueUrl: config.queueUrl
+  };
+
+  sqs.sendMessage(params, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.MessageId);
+    }
+  });
+}
+
+sendMessage('YOUR_MESSAGE');
+
